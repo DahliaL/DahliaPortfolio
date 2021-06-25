58006f4284bab09263e9707bfc1e6525a95d1f9a
DahliaL <dahlia.lapom@gmail.com>
2021-06-25 15:44:02 -0400

    restarting + home

--- src/App.js
+++ src/App.js
@@ -1,12 +1,64 @@
-import React from 'react';
-import './App.css';
-import Home from "./components/home";
+import { React, useState } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
 
-function App() {
+// Components :)
+import NavMenu from './components/NavMenu';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Projects from './pages/Projects';
+import Welcome from './pages/Welcome';
+import Error from './pages/Error';
+
+const LightTheme = {
+  darkBG: '#ffffff',
+  deepBG: '#d1d1d1',
+  grayBG: '#9ccfd9',
+  activeBG: '#9ccfd9',
+};
+
+const DarkTheme = {
+  darkBG: '#262626;',
+  deepBG: '#1E1E1E',
+  grayBG: '#BCB4B4',
+  activeBG: '#363636',
+};
+
+const themes = {
+  light: LightTheme,
+  dark: DarkTheme,
+};
+
+export default function App({ setThemeU }) {
+  const [theme, setTheme = setThemeU] = useState('light');
+
   return (
-    
-    <Home/>
+    <ThemeProvider theme={themes[theme]}>
+      <Router>
+        <NavMenu theme={theme} setTheme={setTheme} />
+        <Switch>
+          <Route path="/" exact>
+            {' '}
+            <Welcome />{' '}
+          </Route>
+          <Route path="/about">
+            {' '}
+            <About />{' '}
+          </Route>
+          <Route path="/projects">
+            {' '}
+            <Projects />{' '}
+          </Route>
+          <Route path="/contact">
+            {' '}
+            <Contact />{' '}
+          </Route>
+          <Route path="/*">
+            {' '}
+            <Error />{' '}
+          </Route>
+        </Switch>
+      </Router>
+    </ThemeProvider>
   );
 }
-
-export default App;
---
+++ src/pages/Welcome.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import WelcomeSection from '../components/Welcome';
+
+export default function Welcome() {
+  return (
+    <div>
+      <WelcomeSection />
+    </div>
+  );
+}
